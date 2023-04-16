23dfff22f6e564280622278ae85cbc1dfc7f420a
Marat <clever1mistory@gmail.com>
2023-04-16 23:30:48 +0300

    Changed the database to PostgreSQL

--- Web/Web/settings.py
+++ Web/Web/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'News_blog',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
